dade75f98047d14bc9af0698c8e76e79a534cccd
nghiaMeo <nghia181032@gmail.com>
2025-09-26 21:19:06 +0700

    feat: verifer and register JWT token

--- auth-service/src/main/java/com/sns/authservice/configuration/JwtConfig.java
+++ auth-service/src/main/java/com/sns/authservice/configuration/JwtConfig.java
@@ -1,11 +1,76 @@
 package com.sns.authservice.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.KeyLengthException;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.sns.authservice.enums.AuthProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Configuration
+import java.text.ParseException;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
 public class JwtConfig {
-    @Bean
-    public
+
+    @Value("${jwt.secret}")
+    private static String SECRET;
+
+    @Value("${jwt.expiration}")
+    private static long expirationMs;
+
+    public static String generateToken(UUID id, String email, String role,
+                                       AuthProvider authProvider) throws Exception {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expirationMs);
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject(String.valueOf(id))
+                .claim("email", email)
+                .claim("authProvider", authProvider)
+                .claim("role", role)
+                .expirationTime(expirationDate)
+                .build();
+        JWSHeader header = new JWSHeader(JWSAlgorithm.ES256);
+
+        SignedJWT signedJWT = new SignedJWT(header, claimsSet);
+        signedJWT.sign(new MACSigner(SECRET.getBytes()));
+        return signedJWT.serialize();
+    }
+
+    public static JWTClaimsSet validateToken(String token) {
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(token);
+            JWSVerifier verifier = new MACVerifier(SECRET.getBytes());
+            if (signedJWT.verify(verifier)) {
+                Date exp = signedJWT.getJWTClaimsSet().getExpirationTime();
+                if (exp.before(new Date())) {
+                    throw new RuntimeException("Expired JWT token");
+                }
+                return signedJWT.getJWTClaimsSet();
+            } else
+                throw new RuntimeException("Invalid JWT token");
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid JWT token " + e.getMessage(), e);
+        }
+    }
+
+    public static String getEmailFromToken(String token) throws ParseException {
+        return validateToken(token).getStringClaim("email");
+    }
+
+
+    public static String getRoleFromToken(String token) throws ParseException {
+        return validateToken(token).getStringClaim("role");
+    }
+
+    public static String getAuthProviderFromToken(String token) throws ParseException {
+        return validateToken(token).getStringClaim("authProvider");
+    }
 
 }
--- auth-service/src/main/java/com/sns/authservice/configuration/SecurityConfig.java
+++ auth-service/src/main/java/com/sns/authservice/configuration/SecurityConfig.java
@@ -2,28 +2,22 @@ package com.sns.authservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.io.InputStream;
-import java.security.KeyFactory;
-import java.security.PublicKey;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(csrf -> csrf.disable())
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/", "/login", "/logout", "/error",
                                 "/oauth/authorize", "/oauth/token").permitAll()
@@ -44,21 +38,5 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public JwtDecoder jwtDecoder() throws Exception {
-        InputStream keyStream = new ClassPathResource("key.pem").getInputStream();
-        String key = new String(keyStream.readAllBytes())
-                .replace("-----BEGIN PUBLIC KEY-----", "")
-                .replace("-----END PUBLIC KEY-----", "")
-                .replaceAll("\\s", "");
-        byte[] decodedKey = Base64.getDecoder().decode(key);
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PublicKey publicKey = keyFactory.generatePublic(keySpec);
-
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
-    }
-
 
 }
